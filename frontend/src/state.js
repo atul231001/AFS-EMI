cb767747bc44f1fd37f63323fccdfa5f35d75b03
DHARMENDRA BAGHELE <dharmendra@fixingdots.in>
2026-08-31 18:20:00 +0530

    change DB5

--- frontend/src/state.js
+++ frontend/src/state.js
@@ -1,6 +1,6 @@
 // src/state.js
 
-const BASE_URL = import.meta.env.DEV ? 'http://localhost:5000/api' : 'https://afs-emi.onrender.com/api';
+const BASE_URL = import.meta.env.DEV ? 'http://localhost:5000/api' : 'https://afs-emi-production.up.railway.app/api';
 import { lightenDarkenColor } from './utils';
 
 const INITIAL_STATE = {
